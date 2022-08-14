eb24eb605955a0f070fb99b5fa41e3ee42290c1c
Vania <63456328+kzmw@users.noreply.github.com>
2022-08-14 23:33:01 +0900

    build

---
+++ build/precache-manifest.07362d84f5d2455bcf3d28a3d8880133.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b34c8c491b7ced357fa1",
+    "url": "/static/css/main.986ce760.chunk.css"
+  },
+  {
+    "revision": "b34c8c491b7ced357fa1",
+    "url": "/static/js/main.b34c8c49.chunk.js"
+  },
+  {
+    "revision": "aea4a5bdfab3b8beece5",
+    "url": "/static/js/1.aea4a5bd.chunk.js"
+  },
+  {
+    "revision": "baf1e2eff514d1d856d9",
+    "url": "/static/css/2.0be21b05.chunk.css"
+  },
+  {
+    "revision": "baf1e2eff514d1d856d9",
+    "url": "/static/js/2.baf1e2ef.chunk.js"
+  },
+  {
+    "revision": "08ab0d562a54b4092b35",
+    "url": "/static/js/runtime~main.08ab0d56.js"
+  },
+  {
+    "revision": "cf519273eef1fb679a76635c173fb0bb",
+    "url": "/index.html"
+  }
+];
