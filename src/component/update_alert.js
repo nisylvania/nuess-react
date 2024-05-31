17aea9cc615f5e4d0f8e6156154068b420f9e2c8
kzmw <kazumanombiri@icloud.com>
2024-05-31 16:58:23 +0900

    update: アラート

--- src/component/update_alert.js
+++ src/component/update_alert.js
@@ -6,7 +6,7 @@ function UpdateAlert() {
 
     let d = new Date();
     let alert = "";
-    if ((d.getMonth() + 1 === 5 || d.getMonth() + 1 === 9) && show) {
+    if ((d.getMonth() + 1 === 3 || d.getMonth() + 1 === 9) && show) {
         alert = <Alert key='danger' variant='danger' onClose={() => setShow(false)} dismissible>
             現在の時期はシラバス更新の時期に当たるため，不正確な情報を表示している可能性があります。
         </Alert>
